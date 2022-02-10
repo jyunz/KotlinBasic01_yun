f276be3424d7e02e633be0b1fdad4a0fdf84883e
Roy <J.jyun0525@gmail.com>
2022-02-10 11:17:00 +0900

    에러로그 찍어보기

--- app/src/main/java/com/example/kotlinbasic01_yun/MainActivity.kt
+++ app/src/main/java/com/example/kotlinbasic01_yun/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.kotlinbasic01_yun
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,7 +11,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         clickBtn.setOnClickListener {
-
+//         이 중괄호 안에 있는 코드만 버튼이 눌렸을때 실행
+            Log.d("메인화면","클릭용 버튼 눌림")
+//            tag는 종류를 나눌때 씀.
+            Log.e("메인화면로그","에러관련로그")
         }
     }
 }
